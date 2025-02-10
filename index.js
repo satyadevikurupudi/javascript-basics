67edb51fcdebbea57ea8dd97f03c97eed62efd8e
satya <kurupudisatyadevi2005@gmail.com>
2025-02-10 20:58:23 +0530

    java script

---
+++ index.js
@@ -0,0 +1,17 @@
+// var a = prompt("enter a value");
+// var b = prompt("enter b value");
+
+// document.write((a+b));
+
+// var a =10;
+// var b=20;
+
+
+// document.write((a+b));
+
+var a =parseInt(prompt("enter a value"));
+var b =parseInt(prompt("enter b value"));
+document.write("addition" +(a+b) +"<br>");
+document.write("subtraction" +(a-b) +"<br>");
+document.write("multiplication" +(a*b) +"<br>");
+document.write("division" +(a/b) +"<br>");
